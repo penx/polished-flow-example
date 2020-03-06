acb5a3967f245340a7cf353427d3027634fd7419
Alasdair McLeay <alasdair.mcleay@me.com>
2020-03-06 15:15:49 +0000

    flow issue with polished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+// @flow
+
+import type { StyledComponent } from 'styled-components';
+
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+import { triangle } from 'polished';
+
 import './App.css';
+
+const MyComponent: StyledComponent<{}, {}, HTMLDivElement> = styled.div`
+  ${triangle({ pointingDirection: 'right', width: '100px', height: '100px', foregroundColor: 'red' })}
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyComponent />
       </header>
     </div>
   );
